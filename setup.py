37053d63a10a84a63a03137ca54be29b8db5b5a9
Mike Yeti <yetisir@gmail.com>
2020-04-20 13:42:27 -0700

    adds kafka docker images

---
+++ setup.py
@@ -0,0 +1,44 @@
+#from setuptools import setup, find_namespace_packages
+#from sphinx.setup_command import BuildDoc
+
+name = 'cryb'
+version = '0.0.1'
+
+setup(
+    name=name,
+    version=version,
+    description='Cryb',
+    url='https://github.com/yetisir/cryb',
+    author='M. Yetisir',
+    author_email='yetisir@gmail.com',
+    license='GNU GPL v3.0',
+#    packages=find_namespace_packages(
+#        include=['data.*', 'geometry.*', 'modelling.*', 'plot.*']),
+    install_requires=[
+ #         'sphinx',
+ #         'sphinx_rtd_theme',
+          'pytest',
+          'pytest-flakes',
+  #        'pyyaml',
+          'configurator',
+  #        'tqdm',
+  #        'pandas',
+  #        'sqlalchemy',
+  #        'psutil',
+          ],
+    zip_safe=False,
+ #   cmdclass={'build_sphinx': BuildDoc},
+    entry_points={
+        'console_scripts': [
+            'cryb = cryb.__main__:main',
+            ],
+        },
+  #  command_options={
+   #     'build_sphinx': {
+    #        'project': ('setup.py', name),
+     #       'version': ('setup.py', version),
+      #      'source_dir': ('setup.py', 'docs'),
+       #     },
+        #}
+    )
+
