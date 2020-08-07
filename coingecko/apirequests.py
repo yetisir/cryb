aa68ac2c5df04e348ed6ed10c44b78a9b19da73e
M. Yetisir <yetisir@gmail.com>
2020-08-06 19:38:42 -0700

    cleanup

--- coingecko/apirequests.py
+++ coingecko/apirequests.py
@@ -11,12 +11,17 @@ import utils
 import config
 
 
+api = pycoingecko.CoinGeckoAPI()
+
+
 class Queue(asyncio.Queue):
-    calls = []
 
     def __init__(self):
 
         super().__init__()
+        self.calls = []
+        self.jobs = {}
+
         self.last_request = time.time()
         self.lock = asyncio.Lock()
 
@@ -55,31 +60,46 @@ class Queue(asyncio.Queue):
             f'API access rate: {rate} requests/{self.rate_timeframe}s')
         return request
 
+    async def add(self, method_name, *args, attempt=1, **kwargs):
+        item = {
+            'method_name': method_name,
+            'args': args,
+            'kwargs': kwargs,
+        }
+        item['id'] = utils.json_hash(item)
+        item['attempt'] = attempt
 
-# TODO: handle globals better
-api = pycoingecko.CoinGeckoAPI()
-queue = Queue()
-jobs = {}
+        self.put_nowait(item)
+
+        self.jobs[item['id']] = {
+            'event': asyncio.Event(),
+            'response': None,
+        }
+        await self.jobs[item['id']]['event'].wait()
+
+        return self.jobs[item['id']]['response']
 
 
-async def add(method_name, *args, attempt=1, **kwargs):
-    item = {
-        'method_name': method_name,
-        'args': args,
-        'kwargs': kwargs,
-    }
-    item['id'] = utils.json_hash(item)
-    item['attempt'] = attempt
+class WorkerPool:
+    queue = Queue()
 
-    queue.put_nowait(item)
+    def __init__(self, workers=None):
+        self.workers = workers
 
-    jobs[item['id']] = {
-        'event': asyncio.Event(),
-        'response': None,
-    }
-    await jobs[item['id']]['event'].wait()
+    async def worker(self):
+        while True:
+            request = await self.queue.get()
 
-    return jobs[item['id']]['response']
+            loop = asyncio.get_event_loop()
+            partial = functools.partial(
+                api_request, request['method_name'], *request['args'], **request['kwargs'])
+            response = await loop.run_in_executor(None, partial)
+            self.queue.jobs[request['id']]['response'] = response
+            self.queue.jobs[request['id']]['event'].set()
+
+    async def run(self):
+        await asyncio.gather(
+            *[self.worker() for _ in range(self.workers)])
 
 
 @utils.cache()
@@ -88,108 +108,10 @@ def api_request(method_name, *args, **kwargs):
     return method(*args, **kwargs)
 
 
-async def worker(name):
-    while True:
-        request = await queue.get()
-
-        loop = asyncio.get_event_loop()
-        partial = functools.partial(
-            api_request, request['method_name'], *request['args'], **request['kwargs'])
-
-        response = await loop.run_in_executor(None, partial)
-        jobs[request['id']]['response'] = response
-        jobs[request['id']]['event'].set()
-
-
 async def run():
-    tasks = []
-    for i in range(config.settings['concurrent_requests']):
-        tasks.append(worker(f'worker-{i}'))
-
-    asyncio.gather(*tasks)
-
-# async def request(method_name, *args, **kwargs):
-#     method=getattr(self.api, method_name)
-#     now=datetime.datetime.utcnow()
-#     request_diff=now - self.last_request
-#     if request_diff < datetime.timedelta(seconds = self.request_delay):
-#         time.sleep(request_diff)
-#     for _ in range(self.max_retries):
-#         try:
-#             result=method(*args, **kwargs)
-#             time.sleep(self.request_delay)
-#             self.last_request=datetime.datetime.utcnow()
-#             return result
-#         except:
-#             time.sleep(self.request_delay)
+    workers = WorkerPool(config.settings['concurrent_requests'])
+    await workers.run()
 
 
-# class Request:
-
-
-#     queue=[]
-
-#     def __init__(self, max_retries = 10, request_delay = 1):
-#         if max_retries is None:
-#             max_retries=config.settings['max_retries']
-#         self.max_retries=max_retries
-
-#         if request_delay is None:
-#             request_delay=config.settings['request_delay']
-#         self.request_delay=request_delay
-
-#     def request(self, method_name, *args, **kwargs):
-#         self.queue.append({
-#             'method_name': method_name,
-#             'args': args,
-#             'kwargs': kwargs,
-#         })
-
-#     def send
-
-#     @utils.cache(ignore_self = True)
-#     def send_request(self, method_name, *args, **kwargs):
-#         method=getattr(self.api, method_name)
-#         now=datetime.datetime.utcnow()
-#         request_diff=now - self.last_request
-#         if request_diff < datetime.timedelta(seconds = self.request_delay):
-#             time.sleep(request_diff)
-#         for _ in range(self.max_retries):
-#             try:
-#                 result=method(*args, **kwargs)
-#                 time.sleep(self.request_delay)
-#                 self.last_request=datetime.datetime.utcnow()
-#                 return result
-#             except:
-#                 time.sleep(self.request_delay)
-
-
-# s
-
-
-# async def worker(name, queue):
-#     while True:
-#         await  # request
-#         queue.task_done()
-
-
-# async def main(n):
-#     queue = asyncio.Queue()
-#     total_sleep_time = 0
-#     for _ in range(20):
-#         sleep_for = 1.5
-#         total_sleep_top += sleep_for
-#         queue.put_nowait(sleep_for)
-
-#     tasks = []
-
-#     for i in range(n):
-#         task = asyncio.create_task(worker(f'worker-{i}', queue))
-#         tasks.append(task)
-
-#     started_at = time.monotonic()
-
-#     await queue.join()
-#     total_slept_for = time.monotonic() - started_at
-
-#     await
+async def add(method_name, *args, **kwargs):
+    return await WorkerPool.queue.add(method_name, *args, **kwargs)
