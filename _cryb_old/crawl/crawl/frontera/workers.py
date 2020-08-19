551c0e060f17ddf6d2e5aae0bc57df171cb59132
M. Yetisir <yetisir@gmail.com>
2020-08-19 16:50:24 -0700

    removing old files

--- _cryb_old/crawl/crawl/frontera/workers.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from .common import *  # noqa
-
-BACKEND = 'frontera.contrib.backends.sqlalchemy.Distributed'
-
-MAX_NEXT_REQUESTS = 2048
-NEW_BATCH_DELAY = 3.0
-
-SQLALCHEMYBACKEND_ENGINE = 'postgres:///10.0.0.223:5432'
